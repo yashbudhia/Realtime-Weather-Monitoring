0890e06eb7ded421662c62a98b3d6c8141f6979e
Yash Budhia <yeshbudhia@gmail.com>
2024-10-25 22:17:39 +0530

    working

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Config:
     OPENWEATHER_API_KEY = os.getenv('WEATHER_API_KEY', 'bd5e378503939ddaee76f12ad7a97608')
     WEATHER_INTERVAL = int(os.getenv('WEATHER_INTERVAL', 300))  # Default to 5 minutes (300 seconds)
     ALERT_THRESHOLD = 35  # Default alert threshold in Celsius
+    MONGO_URI = 'mongodb://localhost:27017/weather_db'
 
